34ca7a2339475f3cc619bd47348d3fed4f59fc6c
jakedemian <jakedemian@gmail.com>
2014-06-30 21:36:28 -0400

    finished navbar and banner

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+function manageNameSize()
+{
+	$('.banner p').css('font-size',($(window).width()*0.08)+'px');    
+}
+
 $(document).ready(function()
 {
+    //initialize window
+    manageNameSize();
     
     // logo effect
     var animationSpeed = 100;
@@ -26,6 +33,28 @@ $(document).ready(function()
         $('.buttonBorder', this).stop().fadeTo(buttonEffect, 0);
     });
     
+    //banner
+    
+    underscoreToggle = 1;
+    
+    setInterval(function() 
+    {
+        if(underscoreToggle == 0)
+        {
+            underscoreToggle = 1;
+            $(".banner p").text('Jake Demian_');
+        }
+        else
+        {
+            underscoreToggle = 0;
+            $(".banner p").text('Jake Demian  ');
+        }
+    }, 500);
+    
+    $(window).resize(function()
+    {
+        manageNameSize();
+    });
     
     
     
