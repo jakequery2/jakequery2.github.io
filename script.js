b5248955bc3dda86d4534d8a482eb92224192f78
jakedemian <jakedemian@gmail.com>
2014-07-01 00:35:47 -0400

    finished main page basic format
    quote and footer, started work on animating the typing of my name but
    its commented out atm

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+var nameHasBeenTyped = false;
+
 function manageNameSize()
 {
 	$('.banner p').css('font-size',($(window).width()*0.08)+'px');    
+}
+
+function typeName()
+{
+    var delayTime = 500;
+    
+    
+//    setTimeout(function(){$(".banner p").text('J_');}, 500);
+//    setTimeout(function(){$(".banner p").text('Ja_');}, 500);
+//    setTimeout(function(){$(".banner p").text('Jak_');}, 500);
+    
+    
+    
+    nameHasBeenTyped = true;
 }
 
 $(document).ready(function()
 {
     //initialize window
     manageNameSize();
+    typeName();
     
     // logo effect
     var animationSpeed = 100;
@@ -34,28 +51,52 @@ $(document).ready(function()
     });
     
     //banner
-    
     underscoreToggle = 1;
     
-    setInterval(function() 
+    if(nameHasBeenTyped)
     {
-        if(underscoreToggle == 0)
+        setInterval(function() 
         {
-            underscoreToggle = 1;
-            $(".banner p").text('Jake Demian_');
-        }
-        else
-        {
-            underscoreToggle = 0;
-            $(".banner p").text('Jake Demian  ');
-        }
-    }, 500);
+            if(underscoreToggle == 0)
+            {
+                underscoreToggle = 1;
+                $(".banner p").text('Jake Demian_');
+            }
+            else
+            {
+                underscoreToggle = 0;
+                $(".banner p").text('Jake Demian  ');
+            }
+        }, 500);
+    }
+    
     
     $(window).resize(function()
     {
         manageNameSize();
     });
     
+    //trigger quote when you scroll far enough
+    $(window).on('scroll', function() {
+        var y_scroll_pos = window.pageYOffset;
+        var scroll_pos_test = 675;             // set to whatever you want it to be
+
+        if(y_scroll_pos > scroll_pos_test) {
+            $('.quote').fadeTo(1200, 1);
+        }
+        
+        if(y_scroll_pos > scroll_pos_test + 30)
+        {
+            $('.credit').fadeTo(1200, 1);
+        }
+    });
     
-    
+    //fade effect for social media links    
+    $('.social-link').hover(function()
+    {
+        $(this).stop().fadeTo(200, 1);
+    }, function()
+    {
+        $(this).stop().fadeTo(200, 0.7);
+    });
 });
